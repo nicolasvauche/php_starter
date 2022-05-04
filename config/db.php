1ab6bbf1454ff07d6d833d2d45c0cd8197a3f4f4
Nicolas vauché <hello@nicolasvauche.net>
2022-05-04 19:39:57 +0200

    Config sample

--- config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Define environment variables for database connection
-putenv('DB_HOST=localhost:8889');
-putenv('DB_NAME=php_starter');
-putenv('DB_USER=nicolas');
-putenv('DB_PASSWORD=nicolas');
